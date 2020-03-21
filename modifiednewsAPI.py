4f5ee90ba59f67b229bd5e0f2d7be7cabf02d1b1
sohammistri <54141739+sohammistri@users.noreply.github.com>
2020-03-21 20:17:43 +0530

    Add files via upload

---
+++ modifiednewsAPI.py
@@ -0,0 +1,8 @@
+from newsapi import NewsApiClient #importing newsAPI
+
+newsapi=NewsApiClient(api_key='39070fd0c030420bb046102d48c0d0ee')
+
+data=newsapi.get_everything(domains='techcrunch.com',language='en')	#getting top headlines from the tech news of India
+
+for value in data['articles']:	#data is a dict, data['articles'] is a list containing the info
+	print(value['title'])		#Only the title i.e the headlines
