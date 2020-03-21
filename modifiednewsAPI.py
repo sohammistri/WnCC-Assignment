b64438742312ffabf046c0e8fe25723738aa9991
sohammistri <54141739+sohammistri@users.noreply.github.com>
2020-03-21 20:32:02 +0530

    Update modifiednewsAPI.py

--- modifiednewsAPI.py
+++ modifiednewsAPI.py
@@ -2,7 +2,7 @@ from newsapi import NewsApiClient #importing newsAPI
 
 newsapi=NewsApiClient(api_key='39070fd0c030420bb046102d48c0d0ee')
 
-data=newsapi.get_everything(domains='techcrunch.com',language='en')	#getting top headlines from the tech news of India
+data=newsapi.get_everything(domains='techcrunch.com',language='en')	#getting top headlines from the techcrunch.com site
 
 for value in data['articles']:	#data is a dict, data['articles'] is a list containing the info
 	print(value['title'])		#Only the title i.e the headlines
